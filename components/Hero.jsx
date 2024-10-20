e264d826f6d920443b106b99b02687c4ea7efe86
Yogeshwar198 <yogeshwaredu198@gmail.com>
2024-10-20 22:46:13 +0530

    first commit

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,33 +1,38 @@
 import React from 'react'
 import Button from './Button';
 
+
 const Hero = () => {
     return (
-        <div className='max-w-full flex flex-col lg:flex-row max-h-screen bg-neutral relative overflow-hidden'>
+        <div className='max-w-full flex flex-col lg:flex-row min-h-screen bg-neutral relative'>
 
             {/* Left div  */}
-            <div className='lg:w-3/5 p-16'>
-                <h1 className="text-secondary text-xl pt-10 cormorant-garamond-bold"><img src="./assets/title_left.svg" className='inline-block' alt="" /> Best Spa Massages Parlor</h1>
-                <p className="text-dark text-6xl xl:text-7xl pt-10 cormorant-garamond-bold">Elevate Your Body
+            <div className='lg:w-3/5 w-full p-8 lg:p-16'>
+                <h1 className="text-secondary text-xl pt-6 cormorant-garamond-bold"><img src="./assets/title_left.svg" className='inline-block' alt="" /> Best Spa Massages Parlor</h1>
+                <p className="text-dark text-4xl sm:text-6xl xl:text-7xl pt-6 lg:pt-10 cormorant-garamond-bold">Elevate Your Body
                     And Mind With Our
                     Spa Massages</p>
 
                 {/* Button component  */}
-                <div className='m-10 w-48'>
+                <div className='mt-8 lg:mt-10 w-48'>
                     <Button name={"EXPLORE SERVICES"} />
                 </div>
 
             </div>
 
             {/* Rotating flower image */}
-            <img className='hidden lg:block w-32 absolute right-1/3 z-10 rotating' src="./assets/flower_1_4.svg" alt="" />
+            <img className='hidden lg:block w-32 absolute left-1/2 z-10 rotating' src="./assets/flower_1_4.svg" alt="" />
             {/* right div  */}
-            <div className='w-1/2 mt-5 bg-neutral-dark relative rounded-b-full'>
-                <div className='bg-neutral-light absolute mt-10 right-0 rounded-b-full'>
-                    <img className='max-w-[38vw]' src="./assets/hero_bg_1_3.png" alt="" />
+            <div className='w-full sm:w-2/3 lg:w-1/2 mt-5 bg-neutral-dark relative rounded-b-full mx-auto flex justify-center items-center lg:justify-end'>
+                <div className='bg-neutral-light rounded-b-full mt-5 max-w-full sm:max-w-md xl:max-w-lg w-full'>
+                    <img className='w-full' src="./assets/hero_bg_1_3.png" alt="" />
                 </div>
             </div>
 
+            <div className="absolute bottom-0 bg-repeat movingX h-16 w-full"
+             style={{ backgroundImage: 'url("/assets/flower_1_4.svg")' }}>
+            
+        </div>
 
         </div>
     )
---
+++ components/WhatWeDo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const WhatWeDo = () => {
+  return (
+    <div>
+        <h1>What We Do</h1>
+    </div>
+  )
+}
+
+export default WhatWeDo
